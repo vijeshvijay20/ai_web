30856a4051552050bd3a0537c96270575469b49f
Vijesh <vijeshvijay20@gmail.com>
2023-10-10 21:35:31 +0530

    sixth commit

--- web_page/urls.py
+++ web_page/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
+     path('yolov8_tracking/', views.yolov8_tracking, name='yolov8_tracking'),
+    path('object_tracked/', views.object_tracked, name='object_tracked'),   
+    path('windows_tracking/',views.windows_tracking,name='windows_tracking'), 
+    path('noise_tracking/',views.noise_tracking,name='noise_tracking'),
+    path('noise_plot/', views.noise_plot, name='noise_plot'),
+   
 ]
--- web_page/views.py
+++ web_page/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
+import json
 
 def index_view(request):
     # Your view logic here
     return render(request, 'web_page/index.html')
+
+def yolov8_tracking(request):
+    # Read JSON data from the file
+    with open('objects_detected_yv8.json', 'r') as json_file:
+        data = json.load(json_file)
+    # Pass the data to the template
+    return render(request, 'web_page/Yolov8_Tracking.html', {'data': data})
+
+def object_tracked(request):
+    with open('objects_detected.json', 'r') as json_file:
+        data = json.load(json_file)        
+    context = {
+        'data': data,
+    }
+    return render(request, 'web_page/object_tracking.html',context)
+
+def windows_tracking(request):
+    # Read JSON data from the file
+    with open('windows_tracking.json', 'r') as json_file:
+        data = json.load(json_file)
+
+    # Pass the data to the template
+    return render(request, 'web_page/windows_tracking.html', {'data': data})
+
+def noise_tracking(request):
+    # Read JSON data from the file
+    with open('noise_data.json', 'r') as json_file:
+        data = json.load(json_file)
+
+    # Pass the data to the template
+    return render(request, 'web_page/noise_tracking.html', {'data': data})
+
+def noise_plot(request):
+    # You don't need to pass any data to this view.
+    return render(request, 'web_page/noise_plot.html')
+
