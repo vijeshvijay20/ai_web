eee6badd090edb37f5df1872d37d9908d32a37a3
Vijesh <vijeshvijay20@gmail.com>
2023-10-10 22:11:18 +0530

    12th commit

--- track_windows.py
+++ track_windows.py
@@ -1,40 +1,32 @@
-import time, os, json
-import pygetwindow as gw
+# track_windows.py
+import gi
+gi.require_version('Wnck', '3.0')
+from gi.repository import Wnck
+import time
+import json
 
-# Initialize variables
-current_window = None
-windows_tracking ={}
+windows_tracking = {}
+
 def empty_windows_tracking_json():
-    if os.path.exists('windows_tracking.json'):
-        with open('windows_tracking.json', 'w') as json_file:
-            json.dump([], json_file)
-            print("Emptied windows_tracking.json.")
-
-def get_current_window():
-    global current_window
-    try:
-        # Get the currently focused window
-        current_window = gw.getActiveWindow()
-        return current_window.title
-    except Exception as e:
-        # Handle exceptions (e.g., no window in focus)
-        return None
+    windows_tracking.clear()
+    print("Emptied windows_tracking.json.")
 
 def track_window_changes():
-    start= True
-    global current_window  # Add this global declaration
-    while start:
-        new_window =  gw.getActiveWindow()
+    screen = Wnck.Screen.get_default()
+    screen.force_update()
+
+    while True:
+        context = time.strftime('%m/%d/%y %H:%M:%S', time.localtime())
+        for window in screen.get_windows():
+            window_title = window.get_name()
+            if window_title not in windows_tracking:
+                windows_tracking[window_title] = []
+
+            windows_tracking[window_title].append(f"Window switched: {context}")
         
-        # Check if the current window has changed
-        if new_window and new_window != current_window:
-            # print(f"Window Changed: {new_window}")
-            current_window = new_window
-            windows_tracking[time.strftime('%m/%d/%y %H:%M:%S', time.localtime())]= "Window switched: {} -> {}".format(new_window, current_window)
-            with open('windows_tracking.json', 'w') as json_file:
-                    json.dump(windows_tracking, json_file, indent=4)
-                    print("Window change registered") 
-        # Sleep for a short interval (e.g., 1 second)
+        with open('windows_tracking.json', 'w') as json_file:
+            json.dump(windows_tracking, json_file, indent=4)
+            print("Window changes registered")
+
         time.sleep(1)
 
-# track_window_changes()
