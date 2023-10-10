c654de561059c5a29396d9324e221446690ec44a
Vijesh <vijeshvijay20@gmail.com>
2023-10-10 19:07:45 +0530

    second commit

--- ai_web/settings.py
+++ ai_web/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = "ai_web.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'ai_web',
+        "USER": 'admin',
+        'PASSWORD': '3idiots',
+        "HOST": 'localhost',
+        "PORT": ""
+
+
     }
 }
 
