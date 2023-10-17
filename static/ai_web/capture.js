41ee43d2583be7470a0832f6a18e1845def64ef1
Vijesh <vijeshvijay20@gmail.com>
2023-10-17 21:21:13 +0530

    capture.js

---
+++ static/ai_web/capture.js
@@ -0,0 +1,9 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const processButton = document.getElementById('processButton');
+const recordedVideo = document.getElementById('recorded');
+
+// Code to access user's webcam and capture video
+// Implement startButton, stopButton, and processButton click event handlers
